dd9ba469220e4a6c4e414398b89af963e928d064
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-10-13 02:06:23 +0300

    Написал метод ввода массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string?[] ReadArrayString()
+{
+    Console.Write("Укажите количество строк, которые хотите ввести: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string?[] arr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
+string?[] Arr = ReadArrayString();
+Console.WriteLine($"{string.Join(", ", Arr)}");
+
