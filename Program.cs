bfb180e06d6ac1fc02203d595351ebe854fde1e9
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-10-13 02:22:20 +0300

    Завершил код программы

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿string?[] ReadArrayString()
+﻿string[] ReadArrayString()
 {
     Console.Write("Укажите количество строк, которые хотите ввести: ");
     int n = Convert.ToInt32(Console.ReadLine());
-    string?[] arr = new string[n];
+    string[] arr = new string[n];
     for (int i = 0; i < n; i++)
     {
         Console.Write("Введите строку №{0}:\r\n    ", i + 1);
@@ -12,6 +12,23 @@
 }
 
 
-string?[] Arr = ReadArrayString();
-Console.WriteLine($"{string.Join(", ", Arr)}");
+string[] ArrayStringLenght(string[] arr)
+{
+    string[] newArr = new String[0];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            Array.Resize(ref newArr, j+1);
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
 
+
+string[] Arr = ReadArrayString();
+string[] NewArr = ArrayStringLenght(Arr);
+Console.WriteLine($"{string.Join(", ", NewArr)}");
