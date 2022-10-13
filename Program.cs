bfa9d8ee6ca4327598df3f17128eecef0e74f92d
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2022-10-13 18:12:05 +0300

    Добавил в код исключения

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-﻿string[] ReadArrayString()
+﻿int lenStr = 4;
+int lenArr = 3;
+string[] ReadArrayString()
 {
-    Console.Write("Укажите количество строк, которые хотите ввести: ");
-    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Укажите количество строк, которые хотите ввести: ");
+    int n;
+    try
+    {
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        n = lenArr;
+        Console.WriteLine($"Вы ввели не число, по умолчанию число строк {n}.");
+    }
     string[] arr = new string[n];
     for (int i = 0; i < n; i++)
     {
         Console.Write("Введите строку №{0}:\r\n    ", i + 1);
         arr[i] = Console.ReadLine();
     }
-    return arr;
+return arr;
 }
 
 
@@ -18,9 +29,9 @@ string[] ArrayStringLenght(string[] arr)
     int j = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length < 4)
+        if (arr[i].Length < lenStr)
         {
-            Array.Resize(ref newArr, j+1);
+            Array.Resize(ref newArr, j + 1);
             newArr[j] = arr[i];
             j++;
         }
@@ -31,4 +42,5 @@ string[] ArrayStringLenght(string[] arr)
 
 string[] Arr = ReadArrayString();
 string[] NewArr = ArrayStringLenght(Arr);
+Console.WriteLine($"Выборка массива строк с длинной не более {lenStr - 1}");
 Console.WriteLine($"{string.Join(", ", NewArr)}");
